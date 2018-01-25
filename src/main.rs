c1bad2d1ee91c980fed3fa9cba262c2bf9d7f7f2
Matthias Krüger <matthias.krueger@famsik.de>
2018-01-25 09:33:01 +0100

    split loop

--- src/main.rs
+++ src/main.rs
@@ -18,40 +18,49 @@ fn main() {
 
     println!("Gathering files...");
 
-    let mut evil_files = Vec::new();
+
+    let mut files_to_check = Vec::new();
 
     for entry in WalkDir::new(dir_to_check) {
         let entry = entry.unwrap();
-        let filename_path = entry.path();
+        let filename_path = entry.path().to_path_buf();
         // don't check directory if directory is named "bla.c/"
         if !filename_path.is_file() {
             continue;
         }
-        let filename_str = format!("{}", filename_path.display());
         match filename_path.extension() {
             Some(ext) => {
                 if ext == "c" || ext == "cl" || ext == "cpp" || ext == "cxx" || ext == "cc"
                     || ext == "c++" || ext == "tpp" || ext == "txx" || ext == "C"
                 {
-                    println!("Checking: {}", &filename_str);
-                    let output = std::process::Command::new(bin)
-                        .arg(&filename_str)
-                        .arg("--enable=all")
-                        .arg("--inconclusive")
-                        .arg("--max-configs=1")
-            //            .arg("--debug")
-            //            .arg("--verbose")
-                        .output()
-                        .expect("failed to run cppcheck!");
-                    if !output.status.success() {
-                        println!("Crash: {}", &filename_str);
-                        evil_files.push(filename_str.clone());
-                    }
+                    files_to_check.push(filename_path.clone());
                 }
             }
             None => continue,
         }
     } // walkdir
+
+
+    let mut evil_files = Vec::new();
+
+    println!("Files gathered");
+    for file in files_to_check {
+        let filename_str = format!("{}", file.display());
+        println!("Checking: {}", &filename_str);
+        let output = std::process::Command::new(bin)
+        .arg(&filename_str)
+        .arg("--enable=all")
+        .arg("--inconclusive")
+        .arg("--max-configs=1")
+//            .arg("--debug")
+//            .arg("--verbose")
+        .output()
+        .expect("failed to run cppcheck!");
+        if !output.status.success() {
+            println!("Crash:        {}", &filename_str);
+            evil_files.push(filename_str.clone());
+        }
+    }
 
     println!("\nCrashing files:");
     for file in evil_files {
